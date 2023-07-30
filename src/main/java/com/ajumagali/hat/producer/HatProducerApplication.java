667bcd79b070fa692178a2a21c665097d61c9940
Asyl Jumagaliyev <ajumagal@jaguarlandrover.com>
2023-07-30 12:30:49 +0100

    initial commit

---
+++ src/main/java/com/ajumagali/hat/producer/HatProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ajumagali.hat.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajumagali/hat/producer/HatProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajumagali.hat.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
